648c905d74ef96fa73d5c9ed461290c37fc84652
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2023-12-25 14:08:14 +0300

    using-setup-api

--- index.php
+++ index.php
@@ -6,11 +6,19 @@ use Bitrix\Main\UI\Extension;
 
 Extension::load('ui.vue3');
 
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script/type/importmap
+//		"ui.vue3": "/bitrix/js/ui/vue3/vue/prod/dist/vue.bundle.min.js"
 ?>
 
-<?php if (0) { ?>
-
+<h3>App1</h3>
 <div id="application"></div>
+<script type="importmap">
+{
+	"imports": {
+		"ui.vue3": "/bitrix/js/ui/vue3/vue/prod/src/vue.js"
+	}
+}
+</script>
 <script type="module">
 import {BitrixVue} from 'ui.vue3';
 BitrixVue.createApp({
@@ -33,8 +41,10 @@ BitrixVue.createApp({
 }).mount('#application');
 </script>
 
-<?php } ?>
+<hr>
 
+
+<h3>App2</h3>
 
 <div id="application2">-1</div>
 
@@ -45,9 +55,27 @@ BitrixVue.createApp({
 </script>
 
 <script type="module">
-	const BitrixVue = BX.Vue3.BitrixVue;
+	//const BitrixVue = BX.Vue3.BitrixVue;
+	const {BitrixVue, h, ref, reactive} = BX.Vue3;
+
+	//console.log(h, ref, reactive);
 
 	BitrixVue.createApp({
+		// new
+		setup() {
+			const counter = ref(0)
+
+			const increment = () => counter.value++
+
+			setInterval(increment, 1000)
+
+			return {
+				counter
+			}
+		},
+
+		// old
+		/*
 		data()
 		{
 			return {
@@ -60,6 +88,8 @@ BitrixVue.createApp({
 				this.counter++
 			}, 1000)
 		},
+		*/
+
 		template: '#app-template2'
 	}).mount('#application2');
 
